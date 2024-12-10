9965d4590565dc98efcfe2198ba2562048c17b8b
Peter Tribble <peter.tribble@gmail.com>
2024-12-10 20:22:10 +0000

    Tidy up verify_iso.sh

--- verify_iso.sh
+++ verify_iso.sh
@@ -18,17 +18,17 @@ case $# in
 	;;
 esac
 
-if [ ! -f ${OVLISO} ]; then
+if [ ! -f "${OVLISO}" ]; then
     echo "ERROR: no ${OVLISO} file"
     exit 1
 fi
 
-for novl in `cat ${OVLISO}`
+for novl in $(cat "${OVLISO}")
 do
-    if [ ! -f ${novl}.ovl ]; then
+    if [ ! -f "${novl}.ovl" ]; then
 	echo "ERROR: missing overlay $novl"
     fi
-    if [ ! -f ${novl}.pkgs ]; then
+    if [ ! -f "${novl}.pkgs" ]; then
 	echo "ERROR: missing pkgs for overlay $novl"
     fi
 done
@@ -41,21 +41,18 @@ if [ ! -d $CKDIR ]; then
     exit 2
 fi
 
-cat ${OVLISO} | sort > ${CKDIR}/list.orig
-cat ${OVLISO} | sort | uniq > ${CKDIR}/list.orig.uniq
+sort "${OVLISO}" > ${CKDIR}/list.orig
+sort -u "${OVLISO}" > ${CKDIR}/list.orig.uniq
 
-DIFF1=`diff ${CKDIR}/list.orig ${CKDIR}/list.orig.uniq`
+DIFF1=$(diff ${CKDIR}/list.orig ${CKDIR}/list.orig.uniq)
 if [ -n "$DIFF1" ]; then
     echo "ERROR: ${OVLISO} has duplicate entries"
 fi
 
-for novl in `cat ${OVLISO}`
-do
-    grep '^REQUIRES=' ${novl}.ovl | awk -F= '{print $2}' >> ${CKDIR}/list.orig.uniq
-done
-cat ${CKDIR}/list.orig.uniq | sort | uniq > ${CKDIR}/list.combo
+grep -h '^REQUIRES=' $(awk '{print $1".ovl"}' "${OVLISO}") | awk -F= '{print $2}' >> ${CKDIR}/list.orig.uniq
+sort -u ${CKDIR}/list.orig.uniq > ${CKDIR}/list.combo
 
-DIFF1=`diff ${CKDIR}/list.orig ${CKDIR}/list.combo`
+DIFF1=$(diff ${CKDIR}/list.orig ${CKDIR}/list.combo)
 if [ -n "$DIFF1" ]; then
     echo "ERROR: ${OVLISO} has missing dependencies"
     diff ${CKDIR}/list.orig ${CKDIR}/list.combo
